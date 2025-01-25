59e58f8105f800fc215b6294b021e9eb99cc5966
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-01-25 16:47:17 +0800

    poooosh

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -5,6 +5,7 @@ const { saveinventoryhistory } = require("../utils/inventorytools")
 const { walletbalance, sendcommissionunilevel, reducewallet } = require("../utils/walletstools")
 const { addanalytics } = require("../utils/analyticstools")
 const { DateTimeServerExpiration, DateTimeServer } = require("../utils/datetimetools")
+const Inventoryhistory = require("../models/Inventoryhistory")
 exports.buytrainer = async (req, res) => {
     const {id, username} = req.user
     const {type, amount } = req.body
@@ -58,7 +59,7 @@ exports.buytrainer = async (req, res) => {
         })
     
         
-        const inventoryhistory = await saveinventoryhistory(id, trainer.name, trainer.rank, `Buy ${trainer.name}`)
+        const inventoryhistory = await saveinventoryhistory(id, trainer.name, trainer.rank, `Buy ${trainer.name}`, amount)
         
         await addanalytics(id, inventoryhistory.data.transactionid, `Buy ${trainer.name}`, `User ${username} bought ${trainer.trainer}`, amount)
 
@@ -163,4 +164,64 @@ exports.getunclaimedincomeinventory = async (req, res) => {
     return res.json({message: "success", data: {
         totalaccumulated: unclaimedincome.length > 0 ? unclaimedincome[0].totalaccumulated : 0
     }})
+}
+exports.getinventoryhistory = async (req, res) => {
+    const {id, username} = req.user
+    const {type, page, limit} = req.query
+
+    const pageOptions = {
+        page: parseInt(page) || 0,
+        limit: parseInt(limit) || 10
+    }
+
+    const history = await Inventoryhistory.find({
+        owner: new mongoose.Types.ObjectId(id),
+        type: { $regex: type, $options: "i" } // Case-insensitive regex search
+    })
+    .skip(pageOptions.page * pageOptions.limit)
+    .limit(pageOptions.limit)
+    .sort({'createdAt': -1})
+    .then(data => data)
+    .catch(err => {
+        console.log(`There's a problem getting the inventory history of ${username}. Error: ${err}`)
+
+        return res.status(400).json({message: "bad-request", data: "There's a problem getting the inventory history. Please contact customer support."})
+    })
+
+    if (history.length <= 0){
+        return res.json({message: "success", data: {
+            history: [],
+            totalpages: 0
+        }})
+    }
+
+    const totalPages = await Inventoryhistory.countDocuments({owner: new mongoose.Types.ObjectId(id), type: type})
+    .then(data => data)
+    .catch(err => {
+
+        console.log(`Failed to count documents inventory history data for ${username}, error: ${err}`)
+
+        return res.status(401).json({ message: 'failed', data: `There's a problem with your account. Please contact customer support for more details` })
+    })
+
+    const pages = Math.ceil(totalPages / pageOptions.limit)
+
+    const data = {
+        history: [],
+        totalpages: pages
+    }
+
+    history.forEach(tempdata => {
+        const {createdAt, rank, trainername, type, amount} = tempdata
+
+        data.history.push({
+            trainername: trainername,
+            rank: rank,
+            type: type,
+            amount: amount,
+            createdAt: createdAt
+        })
+    })
+
+    return res.json({message: "success", data: data})
 }
--- routes/inventory.js
+++ routes/inventory.js
@@ -1,4 +1,4 @@
-const { getunclaimedincomeinventory, buytrainer, getinventory } = require("../controllers/inventory")
+const { getunclaimedincomeinventory, buytrainer, getinventory, getinventoryhistory } = require("../controllers/inventory")
 const { protectplayer } = require("../middleware/middleware")
 
 const router = require("express").Router()
@@ -7,6 +7,7 @@ const router = require("express").Router()
 router 
 .get("/getunclaimedincomeinventory", protectplayer, getunclaimedincomeinventory)
 .get("/getinventory", protectplayer, getinventory)
+.get("/getinventoryhistory", protectplayer, getinventoryhistory)
 .post("/buytrainer", protectplayer, buytrainer)
 
 module.exports = router
