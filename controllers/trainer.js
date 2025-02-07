abba7e5486871e27bf3aca2414b3b6111b6d2e12
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-02-07 11:02:06 +0800

    pooosh

--- controllers/trainer.js
+++ controllers/trainer.js
@@ -1,5 +1,6 @@
 const { default: mongoose } = require("mongoose")
 const Trainer = require("../models/Trainer")
+const Inventoryhistory = require("../models/Inventoryhistory")
 
 
 exports.getTrainers = async(req, res)=> {
@@ -73,6 +74,56 @@ exports.edittrainer = async (req, res) => {
         console.log(`There's a problem encountered while updating ${trainerid} mole. Error: ${err}`)
         return res.status(400).json({ message: "bad-request", data: "There's a problem with the server. Please contact customer support for more details."})
     })
+
+    return res.status(200).json({ message: "success" })
+}
+
+
+
+exports.getusertrainer = async (req, res) => {
+    const { id, username } = req.user
+
+    const { type } = req.query
+
+
+    if (!type) {
+        return res.status(400).json({ message: "failed", data: "Incomplete form data." });
+    }
+
+    if(type === 'Expert'){
+        const test1 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Novice" })
+
+        if(!test1){
+            return res.status(400).json({ message: "failed", data: `You need to claim a Novice (1) Trainer first.` });
+        }
+    } else if (type === 'Ace'){
+        const test1 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Novice" })
+        const test2 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Expert" })
+
+        if(!test1 || !test2){
+            return res.status(400).json({ message: "failed", data: `You need to claim a Novice (1) and Expert (1) Trainer first.` });
+        }
+    } else if (type === 'Ace of Spade'){
+        const test1 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Novice" })
+        const test2 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Expert" })
+        const test3 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Ace" })
+
+        if(!test1 || !test2 || !test3){
+            return res.status(400).json({ message: "failed", data: `You need to claim a Novice (1), Expert (1) and Ace (1) Trainer first.` });
+        }
+    }  else if (type === 'Ace of Heart'){
+        const test1 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Novice" })
+        const test2 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Expert" })
+        const test3 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Ace" })
+        const test4 = await Inventoryhistory.findOne({ owner: new mongoose.Types.ObjectId(id), type: { $regex: /^Claim/ }, rank: "Ace of Spade" })
+
+        if(!test1 || !test2 || !test3 || !test4){
+            return res.status(400).json({ message: "failed", data: `You need to claim a Novice (1), Expert (1), Ace (1) and Ace of Spade (1) Trainer first.` });
+        }
+    } else {
+        return res.status(400).json({ message: "failed", data: "Invalid type." });
+    }
+
 
     return res.status(200).json({ message: "success" })
 }
--- routes/trainer.js
+++ routes/trainer.js
@@ -1,4 +1,4 @@
-const { getTrainers, edittrainer } = require("../controllers/trainer")
+const { getTrainers, edittrainer, getusertrainer } = require("../controllers/trainer")
 const { protectplayer, protectsuperadmin } = require("../middleware/middleware")
 
 const router = require("express").Router()
@@ -7,6 +7,7 @@ const router = require("express").Router()
 router
 .get("/gettrainers", protectplayer, getTrainers)
 .get("/gettrainersadmin", protectsuperadmin, getTrainers)
+.get("/getusertrainer", protectplayer, getusertrainer)
 .post("/edittrainer", protectsuperadmin, edittrainer)
 
 module.exports = router
