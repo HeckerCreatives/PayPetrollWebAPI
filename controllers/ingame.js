a5e0d58ec5e4af93cd171d7111f6eb0a4e03abc9
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-06-02 17:56:05 +0800

    pooosh

--- controllers/ingame.js
+++ controllers/ingame.js
@@ -4,6 +4,7 @@ const Evententrylimit = require("../models/Evententrylimit")
 const Eventtierentry = require("../models/Eventtierentry")
 const { default: mongoose } = require("mongoose")
 const Eventtimelimit = require("../models/Eventtimelimit")
+const Playerevententrylimit = require("../models/Playerevententrylimit")
 
 exports.getingamelist = async (req, res) => {
     const {id, username} = req.user
@@ -107,6 +108,11 @@ exports.saveplayerentrylimit = async (req, res) => {
 
 
   await Evententrylimit.findOneAndUpdate({_id: new mongoose.Types.ObjectId(limit[0]._id)}, {limit: entrylimit})
+  .catch(err => {
+    console.log(err)
+  })
+
+  await Playerevententrylimit.updateMany({}, {limit: entrylimit})
   .catch(err => {
     console.log(err)
   })
